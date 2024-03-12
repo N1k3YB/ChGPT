325f602d82c87aa2cd200c290f4041f421afb22d
N1k3YB <123145685+N1k3YB@users.noreply.github.com>
2024-03-12 06:38:35 +0500

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ async function sendMessage(message) {
 
     const data = { message };
     try {
-      const response = await fetch('https://chgptconn.onrender.com/generate_response', {
+      const response = await fetch('https://127.0.0.1:8000/generate_response', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
